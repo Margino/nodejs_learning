fe12c8ee89c5fc4521fa4e705a8df67b6dcd2871
Alex Petrov <i@petrov.com.ru>
2018-12-09 01:27:41 +0300

    requiring your own files

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-console.log('app running.');
+console.log('running app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
 
-const userInfo = os.userInfo();
+const result = notes.add(9, -2);
+console.log(result);
 
-fs.appendFileSync('greeting.txt', `Hello ${userInfo.username}!`);
+// const res = notes.addNote();
+// console.log(res);
+
+// const userInfo = os.userInfo();
+// fs.appendFileSync('greeting.txt', `Hello ${userInfo.username}! You are ${notes.age}.`);
