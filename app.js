36053f1a13b59e91c0b0f4cbdefafdef1f13706d
Alex Petrov <i@petrov.com.ru>
2018-12-09 20:45:34 +0300

    simplified imput with yargs

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ console.log('running app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-let command = process.argv[2];
-console.log(command);
+let argv = yargs.argv;
+let command = argv._[0]
+console.log('Command:', command);
+console.log('Yargs:', argv);
 
 if (command === 'add') {
-    console.log('Add new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Reading note');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Removeing note');
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
