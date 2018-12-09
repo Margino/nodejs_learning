d4326583fef057026301815f728d8cc48e6e206a
Alex Petrov <i@petrov.com.ru>
2018-12-09 11:11:40 +0300

    using 3d party modules

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ console.log('running app.js');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-const result = notes.add(9, -2);
-console.log(result);
+
+console.log(_.isString('abc'));
+console.log(_.isString(true));
+
+const filteredArray = _.uniq(['Alex', 1, 4, 1, 8, 'Alex']);
+console.log(filteredArray);
+
+
+// const result = notes.add(9, -2);
+// console.log(result);
+
 
 // const res = notes.addNote();
 // console.log(res);
+
 
 // const userInfo = os.userInfo();
 // fs.appendFileSync('greeting.txt', `Hello ${userInfo.username}! You are ${notes.age}.`);
