06c7a00893d4ebac7bd1d2f7554c9c09c2e1a60d
Alex Petrov <i@petrov.com.ru>
2018-12-19 12:09:10 +0300

    pretty printing objects

--- weather/app.js
+++ weather/app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://raw.githubusercontent.com/Margino/nodejs_learning/weather/weather/playground/data.json',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 1));
 });
