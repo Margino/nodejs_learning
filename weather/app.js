9a80b3a3bbcd44244c38ba67dbefdc8285d8f954
Alex Petrov <i@petrov.com.ru>
2018-12-20 13:22:22 +0300

    enicoding user input

--- weather/app.js
+++ weather/app.js
@@ -1,11 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            descrive: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const encoddedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://raw.githubusercontent.com/Margino/nodejs_learning/weather/weather/playground/data.json',
+    url: `https://raw.githubusercontent.com/Margino/nodejs_learning/weather/weather/playground/data.json?address=${encoddedAddress}`,
     json: true
 }, (error, response, body) => {
-    // console.log(JSON.stringify(body, undefined, 1));
+    console.log('---');
+
+    console.log(`EncodeURI: ${JSON.stringify(response.request.uri.query, undefined, 1)}`);
+    
+    console.log(`DecodeURI: ${decodeURIComponent(JSON.stringify(response.request.uri.query, undefined, 1))}\n`);
+
     console.log(`Adress: ${body.results[0].formatted_address}`);
     console.log(`Altitude: ${body.results[0].geometry.location.lat}`);
     console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+
+    console.log('---');
 });
