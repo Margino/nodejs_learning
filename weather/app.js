ccc40039a77d5a5717c27ca1afa723c19e57482f
Alex Petrov <i@petrov.com.ru>
2018-12-20 10:59:12 +0300

    what is make up an http request

--- weather/app.js
+++ weather/app.js
@@ -4,5 +4,8 @@ request({
     url: 'https://raw.githubusercontent.com/Margino/nodejs_learning/weather/weather/playground/data.json',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 1));
+    // console.log(JSON.stringify(body, undefined, 1));
+    console.log(`Adress: ${body.results[0].formatted_address}`);
+    console.log(`Altitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
