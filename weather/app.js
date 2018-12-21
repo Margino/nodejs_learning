db64b255f45754314c7851d9b711c6b3e7a72369
Alex Petrov <i@petrov.com.ru>
2018-12-21 14:31:14 +0300

    callback errors

--- weather/app.js
+++ weather/app.js
@@ -17,18 +17,16 @@ const argv = yargs
 const encoddedAddress = encodeURIComponent(argv.address);
 
 request({
-    url: `https://raw.githubusercontent.com/Margino/nodejs_learning/weather/weather/playground/data.json?address=${encoddedAddress}`,
+    url: `https://raw.githubusercontent.com/Margino/nodejs_learning/weather/weather/playground/data${encoddedAddress}.json`,
     json: true
 }, (error, response, body) => {
-    console.log('---');
-
-    console.log(`EncodeURI: ${JSON.stringify(response.request.uri.query, undefined, 1)}`);
-    
-    console.log(`DecodeURI: ${decodeURIComponent(JSON.stringify(response.request.uri.query, undefined, 1))}\n`);
-
-    console.log(`Adress: ${body.results[0].formatted_address}`);
-    console.log(`Altitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-
-    console.log('---');
+    if (error) {
+        console.log('Unable to connect to the service.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find this address.');
+    } else if (body.status === 'OK') {
+        console.log(`Adress: ${body.results[0].formatted_address}`);
+        console.log(`Altitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
