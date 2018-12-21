94a2d6cebd81e0391c9e63717dc74782fa20c65f
Alex Petrov <i@petrov.com.ru>
2018-12-21 22:20:42 +0300

    chaining callbacks together

--- weather/app.js
+++ weather/app.js
@@ -1,36 +1,31 @@
-// const yargs = require('yargs');
-// const geocode = require('./geocode/geocode.js');
-//
-// const argv = yargs
-//     .options({
-//         a: {
-//             demand: true,
-//             alias: 'address',
-//             descrive: 'Address to fetch weather for',
-//             string: true
-//         }
-//     })
-//     .help()
-//     .alias('help', 'h')
-//     .argv;
-//
-// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-//     if (errorMessage) {
-//         condole.log(errorMessage);
-//     } else {
-//         console.log(JSON.stringify(results, undefined, 2));
-//     }
-// });
+const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
-const request = require('request');
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            descrive: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
-const weater = request({
-    url: 'https://api.darksky.net/forecast/0cddcade1c74aef9741f9c9204baf19b/37.4261631,-122.07608',
-    json: true
-}, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-        console.log(JSON.stringify(`Temperature: ${body.currently.temperature}`));
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
     } else {
-        console.log('Unable to connect to the forecast service.');
+        console.log(results.address);
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if (errorMessage) {
+                console.log(errorMessage);
+            } else {
+                console.log(`The temperature is ${weatherResults.temperature} but it feels like ${weatherResults.apprentTemperature}.`);
+            }
+        });
     }
 });
