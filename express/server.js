78ccdd70f4d20882eaa41394ddec211d008a54d6
Alex Petrov <i@petrov.com.ru>
2018-12-24 11:41:15 +0300

    express middleware

--- express/server.js
+++ express/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const fs = require('fs');
 const hbs = require('hbs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partial');
 app.set('view engin', 'hbs');
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFileSync('server.log', `${log}\n`);
+    console.log(log);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
