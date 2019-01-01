d2f14383a6a9703a3eeff18e30737e0fd12db38c
Alex Petrov <i@petrov.com.ru>
2019-01-01 22:10:35 +0300

    organizing test with describe()

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -3,32 +3,39 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-it('should return Hello world!', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toMatchObject({
-                error: 'Page not found'
-            })
-        })
-        .end(done)
-});
+describe('Server', () => {
 
-
-it('Should return my users object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toEqual(
-                expect.arrayContaining([
-                    expect.objectContaining({
-                        name: 'John',
-                        age: 22,
+    describe('Get /', () => {
+        it('should return Hello world!', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toMatchObject({
+                        error: 'Page not found'
                     })
-                ])
-            )
-        })
-        .end(done)
+                })
+                .end(done)
+        });
+    });
+
+    describe('Get /users', () => {
+        it('Should return my users object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toEqual(
+                        expect.arrayContaining([
+                            expect.objectContaining({
+                                name: 'John',
+                                age: 22,
+                            })
+                        ])
+                    )
+                })
+                .end(done)
+        });
+    });
+
 });
--- test/utils/utils.test.js
+++ test/utils/utils.test.js
@@ -1,33 +1,39 @@
 const expect = require('expect');
 const utils = require('./utils.js');
 
-it('should add two numbers', () => {
-    const res = utils.add(33, 11);
+describe('Utils', () => {
 
-    expect(res).toBe(44);
-    expect(typeof res).toBe('number');
-});
+    describe('#add', () => {
+        it('should add two numbers', () => {
+            const res = utils.add(33, 11);
 
-it('should async add to number', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7);
-        expect(typeof sum).toBe('number');
-        done();
-    })
-});
+            expect(res).toBe(44);
+            expect(typeof res).toBe('number');
+        });
+    });
 
-it('should square a number', () => {
-    const res = utils.square(3);
 
-    expect(res).toBe(9);
-    expect(typeof res).toBe('number');
-});
+    it('should async add to number', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7);
+            expect(typeof sum).toBe('number');
+            done();
+        })
+    });
 
-it ('should async square a number', (done) => {
-    utils.asyncSquare(5, (res) => {
-        expect(res).toBe(25);
+    it('should square a number', () => {
+        const res = utils.square(3);
+
+        expect(res).toBe(9);
         expect(typeof res).toBe('number');
-        done();
+    });
+
+    it ('should async square a number', (done) => {
+        utils.asyncSquare(5, (res) => {
+            expect(res).toBe(25);
+            expect(typeof res).toBe('number');
+            done();
+        });
     });
 });
 
