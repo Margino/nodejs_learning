f9a7cb0139d4ad776b08f95698723231dd763412
Alex Petrov <i@petrov.com.ru>
2019-01-03 19:14:50 +0300

    the objectid

--- todo/playground/mongodb-connect.js
+++ todo/playground/mongodb-connect.js
@@ -1,11 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+const obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, db) => {
     if (err) {
         return console.log('Unable to connect to MongoDB server', err);
     }
     console.log('Connected to MongoDB server');
-
 
     // const todoDb = db.db('Todos');
     //
@@ -20,19 +23,19 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    const userDb = db.db('User');
-
-    userDb.collection('User').insertOne({
-        name: 'Alex',
-        age: 22,
-        location: 'Philadelphia'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user', err);
-        }
-
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    // const userDb = db.db('User');
+    //
+    // userDb.collection('User').insertOne({
+    //     name: 'Alex',
+    //     age: 22,
+    //     location: 'Philadelphia'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert user', err);
+    //     }
+    //
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+    // });
 
     db.close();
 });
